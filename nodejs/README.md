98ef0421109718779304fbdd5ed7ac7f8473eae4
Water Pro <@>
2017-11-25 03:45:03 +0000

    Added ReadMe file

---
+++ nodejs/README.md
@@ -0,0 +1,4 @@
+# Node.js Installation on CentOS
+
+The [scripts](https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions "Enterprise Linux and Fedora ") put nodejs input the package manager's source list. 
+
---
+++ nodejs/getsetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+################################################################################
+########## As of November 2017, the Long Term Support (LTS) version   ##########
+########## is 8.x Carbon                                              ##########
+########## See: https://github.com/nodejs/Release#release-schedule    ##########
+################################################################################
+
+############################################################
+########## https://deb.nodesource.com/setup_8.x   ##########
+########## https://deb.nodesource.com/setup_9.x   ##########
+########## https://rpm.nodesource.com/setup_8.x   ##########
+########## https://rpm.nodesource.com/setup_9.x   ##########
+############################################################
+
+curl --location --remote-name https://rpm.nodesource.com/setup_8.x
+
