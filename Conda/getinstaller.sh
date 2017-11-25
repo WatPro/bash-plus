243430c46325cc7b300c7dbf321dfc8d309d96d0
Water Pro <@>
2017-11-25 05:48:23 +0000

    Updated

--- Conda/getinstaller.sh
+++ Conda/getinstaller.sh
@@ -6,8 +6,11 @@
 ################################################################################
   
 curl --remote-name https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-curl --remote-name https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86.sh
 curl --remote-name https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+
+if [ "$1" = 'all' ] 
+then 
+curl --remote-name https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86.sh
 curl --remote-name https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86.sh
 curl --remote-name https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-ppc64le.sh
 curl --remote-name https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-ppc64le.sh
@@ -18,6 +21,7 @@ curl --remote-name https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86.s
 curl --remote-name https://repo.continuum.io/archive/Anaconda2-5.0.1-Linux-x86_64.sh
 curl --remote-name https://repo.continuum.io/archive/Anaconda2-5.0.0-Linux-ppc64le.sh
 curl --remote-name https://repo.continuum.io/archive/Anaconda2-5.0.1-Linux-x86.sh
+fi
 
 
 
